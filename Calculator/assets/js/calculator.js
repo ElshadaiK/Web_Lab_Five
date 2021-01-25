61d9738602d2106f833ca1a61c82a205bedd710d
Elshadai Tegegn <elshadaikassutegegn@gmail.com>
2021-01-25 15:21:36 +0300

    Calcultor adjusted

--- Calculator/assets/js/calculator.js
+++ Calculator/assets/js/calculator.js
@@ -1,3 +1,90 @@
+let input = document.getElementById("input");
+let btns = document.querySelectorAll(".btn");
+let calculations = document.getElementById("calculation");
+let results = document.getElementById("result");
+let i = btns.length
+let first_item = true
+while(i--){
+    btns[i].addEventListener("click", calculate)
+}
+function calculate(e){
+    let the_target_name = e.target.name
+    let the_input_value = input.value
+    if(the_target_name == "+" || the_target_name == "-" || the_target_name == "*" || the_target_name == "/"|| 
+        the_target_name == "%"|| the_target_name == "//" || 
+        the_target_name == "pow"|| the_target_name == "sqrt" ){
+        if(input.value){
+            if(first_item){
+                
+                if(the_target_name == "pow" || the_target_name == "sqrt"){
+                    calculations.value = the_target_name + " " + String(the_input_value)
+                    results.value = (the_target_name == "pow" )? Math.pow(Number(the_input_value), 2) : Math.sqrt(Number(the_input_value))
+                }
+                else{
+                    calculations.value = String(the_input_value)
+                    results.value = the_input_value
+                    }
+                input.value = ""
+                first_item = false
+            }
+            else{
+                if(the_target_name == "pow" || the_target_name == "sqrt"){
+                    calculations.value = the_target_name + " " + String(the_input_value)
+                }
+                else{
+                    calculations.value = calculations.value + the_target_name + String(the_input_value)
+                }
+                input.value = ""
+            
+                switch(the_target_name){
+                    case "+":
+                        results.value = add([results.value, the_input_value])
+                        break;
+                    case "-":
+                        results.value = sub(results.value, the_input_value)
+                        break;
+                    case "*":
+                        results.value = mult([results.value, the_input_value])
+                        break;
+                    case "/":
+                        results.value = div(results.value, the_input_value)
+                        break;
+                    case "%":
+                        results.value = mod(results.value, the_input_value)
+                        break;
+                    case "//":
+                        results.value = int_div(results.value, the_input_value)
+                        break;
+                    case "pow":
+                        results.value = Math.pow(Number(results.value), 2)
+                        break;
+                    case "sqrt":
+                        results.value = Math.sqrt(Number(results.value))
+                        break;
+                }
+            }
+        }
+    }
+    else{
+        if(the_target_name == "c"){
+            first_item = true;
+            calculations.value = "";
+            results.value = "";
+        }
+        else{
+            if(first_item){
+                calculations.value = String(the_input_value)
+                results.value = the_input_value
+                first_item = false
+            }
+            else{
+                alert("Your result is shown below or you haven't chosen operation")
+            }
+        }
+        input.value = ""
+    }
+
+}
 // Calculator
 
 let add = function(param){
@@ -20,50 +107,18 @@ let sub = function(param1, param2){
 }
 let div = function(param1, param2){
     if(param2 == 0){
+        first_item = true
         return "DIVISION BY ZERO"
     } 
     return Number(param1) / Number(param2);
 }
-
-let choice = prompt("Enter your operation");
-
-let operation;
-(function() {
-    switch(choice){
-        case "+": 
-            operation = add;
-            break;
-        case "-":
-            operation = sub
-            break;
-        case "/":
-            operation = div
-            break;
-        case "*":
-            operation = mult
-            break;
-        default:
-            console.log("Please enter a valid operation")
-        
-    }
-    console.log("**************************************************************")
-    if(operation){
-        let all_numbers = true;
-        let userInput = prompt("Enter your numbers, separated by space");
-        let params = userInput.split(" ")
-        params.forEach(element => {
-            if(typeof(Number(element)) == NaN) all_numbers = false;
-        });
-        if(!all_numbers){
-            console.log("Enter numbers only please")
-        }
-        else{
-            let res;
-            if(operation == div || operation == sub){res = operation(...params)}
-            else{ res = (operation(params))}
-            console.log(res)
-        }
-    }
-    console.log("**************************************************************")
-
-})();
+let mod = function(param1, param2){
+    if(param2 == 0){
+        first_item = true
+        return "DIVISION BY ZERO"
+    } 
+    return Number(param1) % Number(param2);
+}
+let int_div = function(param1, param2){
+    return Math.floor(Number(param1) / Number(param2));
+}
