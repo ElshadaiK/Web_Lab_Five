0f0e9995550bb7faa68f475bf7a4f263f4cf8a7f
Elshadai Tegegn <elshadaikassutegegn@gmail.com>
2021-02-02 10:28:33 +0300

    Multiple operands supported

--- Calculator/assets/js/calculator.js
+++ Calculator/assets/js/calculator.js
@@ -1,42 +1,44 @@
 let input = document.getElementById("input");
+let input2 = document.getElementById("input2")
 let btns = document.querySelectorAll(".btn");
 let calculations = document.getElementById("calculation");
 let results = document.getElementById("result");
+let results2 = document.getElementById("result2");
 let i = btns.length
 let first_item = true
-while(i--){
+while (i--) {
     btns[i].addEventListener("click", calculate)
 }
-function calculate(e){
+function calculate(e) {
     let the_target_name = e.target.name
     let the_input_value = input.value
-    if(the_target_name == "+" || the_target_name == "-" || the_target_name == "*" || the_target_name == "/"|| 
-        the_target_name == "%"|| the_target_name == "//" || 
-        the_target_name == "pow"|| the_target_name == "sqrt" ){
-        if(input.value){
-            if(first_item){
-                
-                if(the_target_name == "pow" || the_target_name == "sqrt"){
+    if (the_target_name == "+" || the_target_name == "-" || the_target_name == "*" || the_target_name == "/" ||
+        the_target_name == "%" || the_target_name == "//" ||
+        the_target_name == "pow" || the_target_name == "sqrt") {
+        if (input.value) {
+            if (first_item) {
+
+                if (the_target_name == "pow" || the_target_name == "sqrt") {
                     calculations.value = the_target_name + " " + String(the_input_value)
-                    results.value = (the_target_name == "pow" )? Math.pow(Number(the_input_value), 2) : Math.sqrt(Number(the_input_value))
+                    results.value = (the_target_name == "pow") ? Math.pow(Number(the_input_value), 2) : Math.sqrt(Number(the_input_value))
                 }
-                else{
+                else {
                     calculations.value = String(the_input_value)
                     results.value = the_input_value
-                    }
+                }
                 input.value = ""
                 first_item = false
             }
-            else{
-                if(the_target_name == "pow" || the_target_name == "sqrt"){
+            else {
+                if (the_target_name == "pow" || the_target_name == "sqrt") {
                     calculations.value = the_target_name + " " + String(the_input_value)
                 }
-                else{
+                else {
                     calculations.value = calculations.value + the_target_name + String(the_input_value)
                 }
                 input.value = ""
-            
-                switch(the_target_name){
+
+                switch (the_target_name) {
                     case "+":
                         results.value = add([results.value, the_input_value])
                         break;
@@ -65,20 +67,44 @@ function calculate(e){
             }
         }
     }
-    else{
-        if(the_target_name == "c"){
+    else {
+        if (the_target_name == "c") {
             first_item = true;
             calculations.value = "";
             results.value = "";
         }
-        else{
-            if(first_item){
-                calculations.value = String(the_input_value)
-                results.value = the_input_value
-                first_item = false
+        else {
+            if (the_target_name == "multiple+" ||  the_target_name == "multiple*" ||  the_target_name == "min" ||  
+            the_target_name == "max") {
+                let the_input_value2 = (input2.value).split(",")
+                if(the_input_value2.length){
+                    switch (the_target_name) {
+                        case "multiple+":
+                            results2.value = add(the_input_value2)
+                            break;
+                        case "multiple*":
+                            results2.value = mult(the_input_value2)
+                            break;
+                        case "min":
+                            results2.value = min(the_input_value2)
+                            break;
+                        case "max":
+                            results2.value = max(the_input_value2)
+                            break;
+                    }
+                }else{
+                    alert("Please enter your numbers separated by comma")
+                }
             }
-            else{
-                alert("Your result is shown below or you haven't chosen operation")
+            else {
+                if (first_item) {
+                    calculations.value = String(the_input_value)
+                    results.value = the_input_value
+                    first_item = false
+                }
+                else {
+                    alert("Your result is shown below or you haven't chosen operation")
+                }
             }
         }
         input.value = ""
@@ -86,39 +112,64 @@ function calculate(e){
 
 }
 // Calculator
-
-let add = function(param){
+let add = function (param) {
     let sum = 0;
-    for(let i=0; i<param.length; i++){
+    for (let i = 0; i < param.length; i++) {
         sum += Number(param[i])
     }
     return sum
 }
-let mult = function(param){
+let mult = function (param) {
     let product = 1;
-    for(let i=0; i<param.length; i++){
+    for (let i = 0; i < param.length; i++) {
         product *= Number(param[i])
     }
     return product
 }
-let sub = function(param1, param2){
+let sub = function (param1, param2) {
     let res = Number(param1) - Number(param2);
     return res
 }
-let div = function(param1, param2){
-    if(param2 == 0){
+let div = function (param1, param2) {
+    if (param2 == 0) {
         first_item = true
         return "DIVISION BY ZERO"
-    } 
+    }
     return Number(param1) / Number(param2);
 }
-let mod = function(param1, param2){
-    if(param2 == 0){
+let mod = function (param1, param2) {
+    if (param2 == 0) {
         first_item = true
         return "DIVISION BY ZERO"
-    } 
+    }
     return Number(param1) % Number(param2);
 }
-let int_div = function(param1, param2){
+let int_div = function (param1, param2) {
     return Math.floor(Number(param1) / Number(param2));
+}
+let min = function (param){
+    let num = null;
+    if(param){
+        num = param[0]
+        for (let index = 0; index < param.length; index++) {
+            if(param[index] < num){
+                num = param[index]
+            }
+            
+        }
+    }
+    return num
+}
+let max = function (param){
+    let num = null;
+    if(param){
+        num = param[0]
+        for (let index = 0; index < param.length; index++) {
+            if(param[index] > num){
+                num = param[index]
+            }
+            
+        }
+    }
+    return num
 }
